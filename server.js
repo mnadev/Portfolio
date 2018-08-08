63159814884f7e828d4ec60d92b507c35ac0c54c
mnadev <mohammednadeem902@gmail.com>
2018-08-08 12:39:41 -0400

    added lnks, more server features

--- server.js
+++ server.js
@@ -5,45 +5,71 @@ const hostname = '127.0.0.1';
 const httpsPort = 443;
 const httpPort = 80;
 
-const mainHTML = fs.readFileSync('index.html')
-const mainCSS = fs.readFileSync('css/index.css')
-const mainJS = fs.readFileSync('js/main.js')
+const mainHTML = fs.readFileSync('index.html');
+const mainCSS = fs.readFileSync('css/index.css');
+const mainJS = fs.readFileSync('js/main.js');
 
-const resumePDF = fs.readFileSync('res/mnadresume.pdf')
-const resumeJSON = fs.readFileSync('res/resume.json')
+const resumePDF = fs.readFileSync('res/mnadresume.pdf');
+const resumeJSON = fs.readFileSync('res/resume.json');
 
-const terminalHTML = fs.readFileSync('html/terminal.html')
-
+const terminalHTML = fs.readFileSync('html/terminal.html');
+const xtermCSS = fs.readFileSync('node_modules/xterm/dist/xterm.css');
+const xtermJS = fs.readFileSync('node_modules/xterm/dist/xterm.js');
+const termJS = fs.readFileSync('js/term_portfolio.js');
 
 const server = http.createServer( (req,res) => {
   console.log(req.url);
   switch (req.url) {
     case '/css/index.css':
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','text/css');
       res.write(mainCSS);
       res.end();
       break;
     case '/js/main.js':
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','text/javascript');
       res.write(mainJS);
       res.end();
       break;
     case '/html/terminal.html':
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','text/html');
       res.write(terminalHTML);
       res.end();
       break;
+    case '/node_modules/xterm/dist/xterm.css':
+      console.log(req.url + "sent");
+      res.writeHead(200, 'Content-Type','text/css');
+      res.write(xtermCSS);
+      res.end();
+      break;
+    case '/node_modules/xterm/dist/xterm.js':
+      console.log(req.url + "sent");
+      res.writeHead(200, 'Content-Type','text/javascript');
+      res.write(xtermJS);
+      res.end();
+      break;
+    case '/js/term_portfolio.js':
+      console.log(req.url + "sent");
+      res.writeHead(200, 'Content-Type','text/javascript');
+      res.write(termJS);
+      res.end();
+      break;
     case '/res/resume.json':
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','application/json');
       res.write(resumeJSON);
       res.end();
       break;
     case '/res/mnadresume.pdf':
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','application/pdf');
       res.write(resumePDF);
       res.end();
       break;
     default:
+      console.log(req.url + "sent");
       res.writeHead(200, 'Content-Type','text/html');
       res.write(mainHTML);
       res.end();
