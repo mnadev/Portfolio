1ae4a0231e661e386b9d1bee344f604408d259cd
mnadev <mohammednadeem902@gmail.com>
2018-08-09 22:46:47 -0400

    changed to express server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const mailer = require('nodemailer');
 
 const hostname = '127.0.0.1';
 const httpsPort = 443;
@@ -17,7 +20,7 @@ const xtermCSS = fs.readFileSync('node_modules/xterm/dist/xterm.css');
 const xtermJS = fs.readFileSync('node_modules/xterm/dist/xterm.js');
 const termJS = fs.readFileSync('js/term_portfolio.js');
 
-const server = http.createServer( (req,res) => {
+app.get('*', (req, res) => {
   console.log(req.url);
   switch (req.url) {
     case '/css/index.css':
@@ -74,16 +77,109 @@ const server = http.createServer( (req,res) => {
       res.write(mainHTML);
       res.end();
   }
-})
+});
 
-server.listen(8080)
+app.listen(
+  3000, () => console.log('Listening on port 3000!'
+));
 
+app.post('*', function(req, res) {
+  let transporter = mailer.createTransport({
+    host:'gmail',
+    auth: {
+      user: 'ksfnksfn',
+      pass: 'skjdnsd'
+    }
+  });
 
-// TODO: HTTPS support, to be added later on
-/*
-http.createServer(
-  res.statusCode = 200;
-  res.setHeader('Content-Type','text/html');
-  res.end(mainHTML);
-).listen(443)
-*/
+  let outMail = {
+    from: email,
+    to: 'mohammednadeem902@gmail.com',
+    subject: req.body.subject,
+    text: "From" + req.body.email + " with name: " + req.body.name + "\n" + req.body.message
+  }
+
+  transport.sendMail(mail, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    transport.close();
+  });
+  res.writeHead(200, 'Content-Type','text/html');
+  res.write(mainHTML);
+  res.end();
+});
+
+// const server = http.createServer( (req,res) => {
+//   console.log(req.url);
+//   switch (req.url) {
+//     case '/css/index.css':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/css');
+//       res.write(mainCSS);
+//       res.end();
+//       break;
+//     case '/js/main.js':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/javascript');
+//       res.write(mainJS);
+//       res.end();
+//       break;
+//     case '/terminal.html':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/html');
+//       res.write(terminalHTML);
+//       res.end();
+//       break;
+//     case '/node_modules/xterm/dist/xterm.css':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/css');
+//       res.write(xtermCSS);
+//       res.end();
+//       break;
+//     case '/node_modules/xterm/dist/xterm.js':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/javascript');
+//       res.write(xtermJS);
+//       res.end();
+//       break;
+//     case '/js/term_portfolio.js':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/javascript');
+//       res.write(termJS);
+//       res.end();
+//       break;
+//     case '/res/resume.json':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','application/json');
+//       res.write(resumeJSON);
+//       res.end();
+//       break;
+//     case '/res/mnadresume.pdf':
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','application/pdf');
+//       res.write(resumePDF);
+//       res.end();
+//       break;
+//     default:
+//       console.log(req.url + "sent");
+//       res.writeHead(200, 'Content-Type','text/html');
+//       res.write(mainHTML);
+//       res.end();
+//   }
+// })
+//
+// server.listen(8080)
+//
+//
+// // TODO: HTTPS support, to be added later on
+// /*
+// http.createServer(
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type','text/html');
+//   res.end(mainHTML);
+// ).listen(443)
+// */
